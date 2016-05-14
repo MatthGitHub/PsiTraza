d0f048556fdffb8869bc052b3c05396c08f8fcc1
MatthGitHub <matiasbenditti@gmail.com>
2016-05-13 21:52:53 -0300

    Agrego estructura BD final

--- psicultura.sql
+++ psicultura.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
+-- version 4.4.3
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 13-05-2016 a las 17:52:34
--- Versión del servidor: 5.5.24-log
--- Versión de PHP: 5.3.13
+-- Tiempo de generación: 14-05-2016 a las 02:51:25
+-- Versión del servidor: 5.6.24
+-- Versión de PHP: 5.6.8
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -28,20 +28,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `clientes` (
   `idCliente` int(11) NOT NULL,
-  `nombre` varchar(50) COLLATE utf8_spanish2_ci NOT NULL,
-  `direccion` varchar(50) COLLATE utf8_spanish2_ci DEFAULT NULL,
-  `telefono` varchar(30) COLLATE utf8_spanish2_ci DEFAULT NULL,
-  `correo` varchar(50) COLLATE utf8_spanish2_ci DEFAULT NULL,
-  PRIMARY KEY (`idCliente`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
---
--- Volcado de datos para la tabla `clientes`
---
-
-INSERT INTO `clientes` (`idCliente`, `nombre`, `direccion`, `telefono`, `correo`) VALUES
-(0, 'pp', 'hjg', '', 'hbgjh@hotmail.com'),
-(1, 'matute', 'lareconn', '', 'sac@cas.com.ar');
+  `nombre` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `direccion` varchar(50) CHARACTER SET latin1 DEFAULT NULL,
+  `telefono` varchar(30) CHARACTER SET latin1 DEFAULT NULL,
+  `correo` varchar(50) CHARACTER SET latin1 DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 -- --------------------------------------------------------
 
@@ -50,34 +41,13 @@ INSERT INTO `clientes` (`idCliente`, `nombre`, `direccion`, `telefono`, `correo`
 --
 
 CREATE TABLE IF NOT EXISTS `depositos` (
-  `iDeposito` int(11) NOT NULL AUTO_INCREMENT,
+  `iDeposito` int(11) NOT NULL,
   `idLote` int(11) NOT NULL,
   `tipoProceso` int(11) NOT NULL,
   `fecha` date NOT NULL,
   `vencimiento` date NOT NULL,
-  `cantidad` decimal(5,2) NOT NULL,
-  PRIMARY KEY (`iDeposito`),
-  KEY `idLote` (`idLote`,`tipoProceso`),
-  KEY `tipoProceso` (`tipoProceso`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=18 ;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `dias_julianos`
---
-
-CREATE TABLE IF NOT EXISTS `dias_julianos` (
-  `dia` int(11) NOT NULL,
-  `fecha` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
---
--- Volcado de datos para la tabla `dias_julianos`
---
-
-INSERT INTO `dias_julianos` (`dia`, `fecha`) VALUES
-(125, '2016-05-04');
+  `cantidad` decimal(5,2) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 -- --------------------------------------------------------
 
@@ -86,18 +56,14 @@ INSERT INTO `dias_julianos` (`dia`, `fecha`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `entregas` (
-  `idEntrega` int(11) NOT NULL AUTO_INCREMENT,
+  `idEntrega` int(11) NOT NULL,
   `cliente` int(11) NOT NULL,
   `tipoProceso` int(11) NOT NULL,
   `fichaExpedicion` varchar(15) CHARACTER SET latin1 NOT NULL,
   `idLote` int(11) NOT NULL,
-  `cantidad` int(11) NOT NULL,
-  `fecha` date NOT NULL,
-  PRIMARY KEY (`idEntrega`),
-  KEY `cliente` (`cliente`,`tipoProceso`,`idLote`),
-  KEY `tipoProceso` (`tipoProceso`),
-  KEY `idLote` (`idLote`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=5 ;
+  `cantidad` decimal(5,2) NOT NULL,
+  `fecha` date NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 -- --------------------------------------------------------
 
@@ -106,13 +72,11 @@ CREATE TABLE IF NOT EXISTS `entregas` (
 --
 
 CREATE TABLE IF NOT EXISTS `ingresos` (
-  `idIngreso` int(11) NOT NULL AUTO_INCREMENT,
+  `idIngreso` int(11) NOT NULL,
   `idLote` int(11) NOT NULL,
   `fecha` date NOT NULL,
-  `cantidad` decimal(5,2) NOT NULL,
-  PRIMARY KEY (`idIngreso`),
-  KEY `idLote` (`idLote`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=12 ;
+  `cantidad` decimal(5,2) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 -- --------------------------------------------------------
 
@@ -122,9 +86,7 @@ CREATE TABLE IF NOT EXISTS `ingresos` (
 
 CREATE TABLE IF NOT EXISTS `lotes` (
   `id_lote` int(11) NOT NULL,
-  `proveedor` int(11) NOT NULL,
-  PRIMARY KEY (`id_lote`),
-  KEY `proveedor` (`proveedor`)
+  `proveedor` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 -- --------------------------------------------------------
@@ -134,15 +96,12 @@ CREATE TABLE IF NOT EXISTS `lotes` (
 --
 
 CREATE TABLE IF NOT EXISTS `procesos` (
-  `idProceso` int(11) NOT NULL AUTO_INCREMENT,
+  `idProceso` int(11) NOT NULL,
   `idLote` int(11) NOT NULL,
   `tipoProceso` int(11) NOT NULL,
   `fecha` date NOT NULL,
-  `cantidad` int(11) NOT NULL,
-  PRIMARY KEY (`idProceso`),
-  KEY `idLote` (`idLote`),
-  KEY `tipoProceso` (`tipoProceso`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=23 ;
+  `cantidad` decimal(5,2) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 -- --------------------------------------------------------
 
@@ -151,21 +110,12 @@ CREATE TABLE IF NOT EXISTS `procesos` (
 --
 
 CREATE TABLE IF NOT EXISTS `proveedores` (
-  `idProveedor` int(11) NOT NULL AUTO_INCREMENT,
+  `idProveedor` int(11) NOT NULL,
   `nombre` varchar(50) CHARACTER SET latin1 NOT NULL,
   `direccion` varchar(50) CHARACTER SET latin1 DEFAULT NULL,
   `telefono` varchar(30) CHARACTER SET latin1 DEFAULT NULL,
-  `correo` varchar(30) COLLATE utf8_spanish2_ci DEFAULT NULL,
-  PRIMARY KEY (`idProveedor`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=3 ;
-
---
--- Volcado de datos para la tabla `proveedores`
---
-
-INSERT INTO `proveedores` (`idProveedor`, `nombre`, `direccion`, `telefono`, `correo`) VALUES
-(1, 'el barto', 'lalala', '', 'sam@gmoil.ar'),
-(2, 'pp', 'nkl', '', 'da@gmail.com');
+  `correo` varchar(50) COLLATE utf8_spanish2_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 -- --------------------------------------------------------
 
@@ -174,21 +124,24 @@ INSERT INTO `proveedores` (`idProveedor`, `nombre`, `direccion`, `telefono`, `co
 --
 
 CREATE TABLE IF NOT EXISTS `tiposprocesos` (
-  `idTipoProceso` int(11) NOT NULL AUTO_INCREMENT,
-  `descripcion` varchar(50) CHARACTER SET latin1 NOT NULL,
-  PRIMARY KEY (`idTipoProceso`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci AUTO_INCREMENT=6 ;
+  `idTipoProceso` int(11) NOT NULL,
+  `descripcion` varchar(50) CHARACTER SET latin1 NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 --
 -- Volcado de datos para la tabla `tiposprocesos`
 --
 
 INSERT INTO `tiposprocesos` (`idTipoProceso`, `descripcion`) VALUES
-(1, 'Sin procesar'),
-(2, 'Desespinado Fresco'),
-(3, 'Desespinado Congelado'),
-(4, 'Eviscerado Fresco'),
-(5, 'Eviscerado Congelado');
+(1, 'Desespinada Fresca'),
+(2, 'Desesperada Congelada'),
+(3, 'Eviscerada Fresca'),
+(4, 'Eviscerada Congelada'),
+(5, 'Fileteada Fresca'),
+(6, 'Fileteada Congelada'),
+(7, 'Fileteada Congelada IQF'),
+(8, 'Fileteada Congelada Block'),
+(9, 'Sin Procesar');
 
 -- --------------------------------------------------------
 
@@ -197,22 +150,133 @@ INSERT INTO `tiposprocesos` (`idTipoProceso`, `descripcion`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `usuarios` (
-  `ID` int(9) unsigned NOT NULL AUTO_INCREMENT,
+  `ID` int(9) unsigned NOT NULL,
   `username` varchar(180) DEFAULT NULL,
   `password` varchar(180) DEFAULT NULL,
   `email` varchar(180) DEFAULT NULL,
   `id_extreme` varchar(180) DEFAULT NULL,
-  `permisos` int(11) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+  `permisos` int(11) NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
 INSERT INTO `usuarios` (`ID`, `username`, `password`, `email`, `id_extreme`, `permisos`) VALUES
-(2, 'mbenditti', '090c36e3bb39377468363197afb3e91b', 'matiasbenditti@gmail.com', NULL, 1);
+(1, 'mbenditti', '090c36e3bb39377468363197afb3e91b', 'matias@gmail.com.ar', NULL, 1),
+(3, 'ale', 'f7aecf1c48e2e8ad0dbc2339ac3bf95a', 'Alebai@gmail.com', NULL, 0);
 
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`idCliente`);
+
+--
+-- Indices de la tabla `depositos`
+--
+ALTER TABLE `depositos`
+  ADD PRIMARY KEY (`iDeposito`),
+  ADD KEY `idLote` (`idLote`,`tipoProceso`),
+  ADD KEY `tipoProceso` (`tipoProceso`);
+
+--
+-- Indices de la tabla `entregas`
+--
+ALTER TABLE `entregas`
+  ADD PRIMARY KEY (`idEntrega`),
+  ADD KEY `cliente` (`cliente`,`tipoProceso`,`idLote`),
+  ADD KEY `tipoProceso` (`tipoProceso`),
+  ADD KEY `idLote` (`idLote`);
+
+--
+-- Indices de la tabla `ingresos`
+--
+ALTER TABLE `ingresos`
+  ADD PRIMARY KEY (`idIngreso`),
+  ADD KEY `idLote` (`idLote`);
+
+--
+-- Indices de la tabla `lotes`
+--
+ALTER TABLE `lotes`
+  ADD PRIMARY KEY (`id_lote`),
+  ADD KEY `proveedor` (`proveedor`);
+
+--
+-- Indices de la tabla `procesos`
+--
+ALTER TABLE `procesos`
+  ADD PRIMARY KEY (`idProceso`),
+  ADD KEY `idLote` (`idLote`),
+  ADD KEY `tipoProceso` (`tipoProceso`);
+
+--
+-- Indices de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  ADD PRIMARY KEY (`idProveedor`);
+
+--
+-- Indices de la tabla `tiposprocesos`
+--
+ALTER TABLE `tiposprocesos`
+  ADD PRIMARY KEY (`idTipoProceso`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `idCliente` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `depositos`
+--
+ALTER TABLE `depositos`
+  MODIFY `iDeposito` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT de la tabla `entregas`
+--
+ALTER TABLE `entregas`
+  MODIFY `idEntrega` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `ingresos`
+--
+ALTER TABLE `ingresos`
+  MODIFY `idIngreso` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
+--
+-- AUTO_INCREMENT de la tabla `procesos`
+--
+ALTER TABLE `procesos`
+  MODIFY `idProceso` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `proveedores`
+--
+ALTER TABLE `proveedores`
+  MODIFY `idProveedor` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT de la tabla `tiposprocesos`
+--
+ALTER TABLE `tiposprocesos`
+  MODIFY `idTipoProceso` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `ID` int(9) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- Restricciones para tablas volcadas
 --
@@ -228,9 +292,9 @@ ALTER TABLE `depositos`
 -- Filtros para la tabla `entregas`
 --
 ALTER TABLE `entregas`
-  ADD CONSTRAINT `entregas_ibfk_1` FOREIGN KEY (`cliente`) REFERENCES `clientes` (`idCliente`),
-  ADD CONSTRAINT `entregas_ibfk_2` FOREIGN KEY (`tipoProceso`) REFERENCES `tiposprocesos` (`idTipoProceso`),
-  ADD CONSTRAINT `entregas_ibfk_3` FOREIGN KEY (`idLote`) REFERENCES `lotes` (`id_lote`);
+  ADD CONSTRAINT `entregas_ibfk_2` FOREIGN KEY (`tipoProceso`) REFERENCES `tiposprocesos` (`idTipoProceso`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `entregas_ibfk_3` FOREIGN KEY (`idLote`) REFERENCES `lotes` (`id_lote`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `entregas_ibfk_4` FOREIGN KEY (`cliente`) REFERENCES `clientes` (`idCliente`);
 
 --
 -- Filtros para la tabla `ingresos`
